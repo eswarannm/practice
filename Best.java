fa330922a545ce7ab344980fad7dcbe5cb96c137
Nithya Eswaran <eswarannm@s.dcsdk12.org>
2018-01-11 12:11:24 -0700

    example and best

---
+++ Best.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+class Best {
+
+	public static void main(String[] args) {
+		int x = Integer.parseInt(args[0]);
+		int y = Integer.parseInt(args[1]);
+
+		String str = "";
+
+		for (int i=0; i<y; i++){
+			str = str + "!";
+		}
+
+		for (int i=0; i<x; i++) {
+			System.out.println("YOU'RE THE BEST" + str);
+		}
+	}
+}
