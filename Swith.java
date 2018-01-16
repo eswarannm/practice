b4ac657b9f860b008cc926da15bc48abaab198ab
Nithya Eswaran <eswarannm@s.dcsdk12.org>
2018-01-16 11:15:43 -0700

    start switch

---
+++ Swith.java
@@ -0,0 +1,13 @@
+class Switch{
+
+	public static void main(String[] args){
+
+	}
+
+	public static String switch(String initial, String swapA, String swapB) {
+		String str = "nojeonajd";
+		str.substring(i, i + swapA.length());
+
+		for (i = 0, i < swapA.length(), i++)
+	}
+}
